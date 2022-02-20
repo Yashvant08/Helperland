287994c01981466823748422208a7c3d7dc41f16
Your Name <yashvantdesai7@gmail.com>
2022-02-20 20:37:05 +0530

    Old api deleted

--- Home screen and Public pages/Contact us/Attachment/attachment.repository.ts
+++
@@ -1,9 +0,0 @@
-// import {db} from "../models/index"
-// import { ContactUsAttachment } from "../models/contactusattachment" 
-
-
-// export class AttachmentRepository{
-//     public async createAttachment(attachments: {[key:number|string]:ContactUsAttachment}): Promise<ContactUsAttachment>{
-//         return db.ContactUsAttachment.create(attachments);
-//     }
-// }
--- Home screen and Public pages/Contact us/public/Attachment/attachment.repository.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import {db} from "../models/index"
-// import { ContactUsAttachment } from "../models/contactusattachment" 
-// export class AttachmentRepository{
-//     public async createAttachment(attachments: {[key:number|string]:ContactUsAttachment}): Promise<ContactUsAttachment>{
-//         return db.ContactUsAttachment.create(attachments);
-//     }
-// }
